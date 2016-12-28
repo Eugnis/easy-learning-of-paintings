c032a2b2fce7821d596f6daa28a25d34b16833ce
Eugnis <eugenijb@gmail.com>
2016-12-28 21:29:42 +0200

    Draw game final

--- app/src/main/java/com/eugnis/easylearningofpaintings/DrawGameActivity.java
+++ app/src/main/java/com/eugnis/easylearningofpaintings/DrawGameActivity.java
@@ -1,16 +1,25 @@
 package com.eugnis.easylearningofpaintings;
 
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.content.res.Configuration;
+import android.graphics.Bitmap;
 import android.graphics.Rect;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.eugnis.easylearningofpaintings.app.App;
+import com.eugnis.easylearningofpaintings.data.model.Painting;
+import com.eugnis.easylearningofpaintings.data.repo.PaintingsRepo;
 import com.flask.colorpicker.ColorPickerView;
 import com.flask.colorpicker.OnColorSelectedListener;
 import com.flask.colorpicker.builder.ColorPickerClickListener;
@@ -18,12 +27,18 @@ import com.flask.colorpicker.builder.ColorPickerDialogBuilder;
 import com.simplify.ink.InkView;
 import com.github.clans.fab.FloatingActionButton;
 
+import java.io.File;
+import java.io.FileOutputStream;
+
 public class DrawGameActivity extends AppCompatActivity {
+    Painting paintingToDraw;
+    Bitmap drawedImage;
+
+    DrawGameSetFragment drawGameSetFragment;
+    DrawGameFragment drawGameFragment;
+    DrawCompareFragment drawCompareFragment;
 
 
-    FloatingActionButton chooseColorBtn;
-    int backgroundColor;
-    InkView ink;
 
 
     @Override
@@ -31,51 +46,48 @@ public class DrawGameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_draw_game);
 
-        ink = ((InkView) findViewById(R.id.ink));
-        chooseColorBtn = (FloatingActionButton) findViewById(R.id.chooseColorBtn);
 
-        backgroundColor = getResources().getColor(android.R.color.black);
-        ink.setColor(backgroundColor);
-        ink.setMinStrokeWidth(1.5f);
-        ink.setMaxStrokeWidth(6f);
-        ink.addFlag(InkView.FLAG_RESPONSIVE_WIDTH);
+        setRandomPainting();
 
-        //chooseColorBtn.setBackgroundColor(backgroundColor);
-        chooseColorBtn.setColorNormal(backgroundColor);
+        drawGameSetFragment = new DrawGameSetFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.drawMainLayout, drawGameSetFragment).commit();
+
 
     }
 
-    public void chooseColor(View view){
-        ColorPickerDialogBuilder
-                .with(this)
-                .setTitle("Виберіть колір")
-                .initialColor(backgroundColor)
-                .wheelType(ColorPickerView.WHEEL_TYPE.FLOWER)
-                .lightnessSliderOnly()
-                .density(12)
-                .setOnColorSelectedListener(new OnColorSelectedListener() {
-                    @Override
-                    public void onColorSelected(int selectedColor) {
-                        //Toast.makeText(getBaseContext(), "onColorSelected: 0x" + Integer.toHexString(selectedColor), Toast.LENGTH_SHORT).show();
-                    }
-                })
-                .setPositiveButton("OK", new ColorPickerClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int selectedColor, Integer[] allColors) {
-                        backgroundColor = selectedColor;
-                        ink.setColor(backgroundColor);
-                        chooseColorBtn.setColorNormal(backgroundColor);
-                    }
-                })
-                .setNegativeButton("Відміна", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                    }
-                })
-                .build()
-                .show();
+    public void startPaint(View v){
+        drawGameFragment = new DrawGameFragment();   // instantiate fragment
+        getSupportFragmentManager().beginTransaction().replace(R.id.drawMainLayout, drawGameFragment).addToBackStack(null).commit();  //  replace original fragment with new fragment, add original to backstack
     }
 
+    public void setRandomPainting(){
+        paintingToDraw = (new PaintingsRepo()).getPainting(null);
+
+    }
+
+    public Painting getPaintingToDraw(){
+        return paintingToDraw;
+    }
+
+    public void setDrawedImage(Bitmap image){
+        this.drawedImage = image;
+
+        getSupportFragmentManager().popBackStackImmediate();
+
+        drawCompareFragment = new DrawCompareFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.drawMainLayout, drawCompareFragment).addToBackStack(null).commit();  //  replace original fragment with new fragment, add original to backstack
+
+    }
+
+    public Bitmap getDrawedImage(){
+        return drawedImage;
+    }
+
+    public void saveDrawToGallery(View v){
+        MediaStore.Images.Media.insertImage(getContentResolver(), this.drawedImage, "Мій рисунок " + this.paintingToDraw.getName() , "Мій рисунок " + this.paintingToDraw.getName());
+        Toast.makeText(v.getContext(), "Збережено!", Toast.LENGTH_SHORT).show();
+
+    }
 
 
 }
